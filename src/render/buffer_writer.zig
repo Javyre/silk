fbdf462f72bb515e790df52e49c1f284bedf3d59
Javier A. Pollak <javi.po.123@gmail.com>
2023-09-14 21:15:16 -0400

    fix: pad writeBuffer calls to be multiples of 4

--- src/render/buffer_writer.zig
+++ src/render/buffer_writer.zig
@@ -15,7 +15,32 @@ pub const GPUBuffer = struct {
         if (self.buffer.getSize() < self.len + bytes.len)
             return error.NoSpaceLeft;
 
-        core.device.getQueue().writeBuffer(self.buffer, self.len, bytes);
+        // NOTE: for some reason, even for uint16 bufffers we need to write a
+        //       multiple of 4 bytes at a time. So we pad the buffer with zeroes
+        const remainder = @rem(bytes.len, 4);
+        if (remainder != 0) {
+            const cutoff = bytes.len - remainder;
+
+            core.device.getQueue().writeBuffer(
+                self.buffer,
+                self.len,
+                bytes[0..cutoff],
+            );
+
+            var buff = std.mem.zeroes([4]u8);
+            for (bytes[cutoff..], 0..) |b, i| {
+                buff[i] = b;
+            }
+
+            core.device.getQueue().writeBuffer(
+                self.buffer,
+                self.len + cutoff,
+                &buff,
+            );
+        } else {
+            core.device.getQueue().writeBuffer(self.buffer, self.len, bytes);
+        }
+
         self.len += bytes.len;
         return bytes.len;
     }
