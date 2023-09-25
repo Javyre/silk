e945ba8ff6b409a829f5e527317376e847c10030
Javier A. Pollak <javi.po.123@gmail.com>
2023-09-24 22:11:32 -0400

    Update to latest mach-core

--- build.zig
+++ build.zig
@@ -5,8 +5,11 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    mach_core.mach_glfw_import_path = "mach_core.mach_glfw";
-    const app = try mach_core.App.init(b, .{
+    const mach_core_dep = b.dependency("mach_core", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const app = try mach_core.App.init(b, mach_core_dep.builder, .{
         .name = "silk",
         .src = "src/main.zig",
         .target = target,
