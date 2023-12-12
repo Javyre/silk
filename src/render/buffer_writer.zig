3d4c625035aa82dd6d07a875ad55f008dbae5fa1
Javier A. Pollak <javi.po.123@gmail.com>
2023-12-12 18:22:49 -0500

    Impl GArrayList & deprecate GPUBuffer
    better typed and more ergonomic.
    no need to follow writer interface as we don't want to write bytes but
    elements.

--- src/render/buffer_writer.zig
+++ src/render/buffer_writer.zig
@@ -2,6 +2,68 @@ const std = @import("std");
 const core = @import("mach-core");
 const gpu = core.gpu;
 
+pub fn GArrayList(comptime T: type) type {
+    return struct {
+        const Error = GPUBuffer.WriteError;
+
+        buffer: GPUBuffer,
+
+        pub fn initCapacity(desc: struct {
+            label: ?[*:0]const u8 = null,
+            usage: gpu.Buffer.UsageFlags,
+            capacity: u64,
+        }) @This() {
+            return .{ .buffer = GPUBuffer{
+                .buffer = core.device.createBuffer(&.{
+                    .size = desc.capacity * @sizeOf(T),
+                    .usage = desc.usage,
+                    .label = desc.label,
+                }),
+            } };
+        }
+
+        pub fn deinit(self: *@This()) void {
+            self.buffer.destroy();
+        }
+
+        pub fn writeOne(self: *@This(), value: T) Error!void {
+            const written = try self.buffer.write(std.mem.asBytes(&value));
+            std.debug.assert(written == @sizeOf(T));
+        }
+
+        pub fn write(self: *@This(), values: []const T) Error!void {
+            const written = try self.buffer.write(std.mem.sliceAsBytes(values));
+            std.debug.assert(written == values.len * @sizeOf(T));
+        }
+
+        /// Get the buffers current physical size in bytes.
+        pub fn getSize(self: *const @This()) u64 {
+            return self.buffer.buffer.getSize();
+        }
+
+        /// Get the current number of elements in the buffer.
+        pub fn getLen(self: *const @This()) u64 {
+            const bytes = self.buffer.len;
+
+            if (std.debug.runtime_safety)
+                std.debug.assert(bytes % @sizeOf(T) == 0);
+
+            return bytes / @sizeOf(T);
+        }
+
+        pub fn getRawBuffer(self: *const @This()) *gpu.Buffer {
+            return self.buffer.buffer;
+        }
+
+        pub fn clear(self: *@This()) void {
+            self.buffer.len = 0;
+        }
+
+        // readAt()?
+    };
+}
+
+/// DEPRECATED: use `GArrayList` instead
 pub const GPUBuffer = struct {
     buffer: *gpu.Buffer,
     len: u64 = 0,
